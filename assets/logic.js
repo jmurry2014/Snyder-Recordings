fd9a704cd5a5490a8ae9f244e2fdfef01d553c4f
James Murry <jmurry2014@gmail.com>
2024-08-30 21:50:35 -0500

    Starting to implement SwipeJs into project for the slider, i was working on this in a seperate file however when i moved it over into the project styling wasnt right, decided to scrap the idea and restart here, emphasis on mobile first approach, was having an issue where the active slider was not centered on the screen was off to the right. This section should be wrapped up tomorrow night

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,16 @@
-function showSideBar() {
-    const sidebar = $('.sidebar');
-    $('.hamburger').click(function() {
-        sidebar.css("display", "flex");
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
     });
-}
-
-function hideSideBar() {
-    const sidebar = $('.sidebar');
-    $('.close').click(function() {
-        sidebar.css("display", "none");
-    });
-}
-showSideBar();
-hideSideBar();
+});
