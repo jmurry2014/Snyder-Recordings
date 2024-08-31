13903a5a6403d790f7bd7310cdff096ac5ebb11f
James Murry <jmurry2014@gmail.com>
2024-08-30 21:54:46 -0500

    accidentally removed my JS for mobile navigation

--- assets/logic.js
+++ assets/logic.js
@@ -14,3 +14,32 @@ document.addEventListener("DOMContentLoaded", function() {
         },
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showSideBar() {
+    const sidebar = $('.sidebar');
+    $('.hamburger').click(function() {
+        sidebar.css("display", "flex");
+    });
+}
+
+function hideSideBar() {
+    const sidebar = $('.sidebar');
+    $('.close').click(function() {
+        sidebar.css("display", "none");
+    });
+}
+showSideBar();
+hideSideBar();
