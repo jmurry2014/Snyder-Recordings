e9d2818e73f1725904260ae96d95fa914d6d8878
James Murry <jmurry2014@gmail.com>
2024-09-04 12:58:16 -0500

    more work on the swiperjs, think im running into bugs, went through my code for any possible styling issues but i ran into this once before in my first iteration, coverflow property isnt capturing the slider i want functionality is there and mobile responsive

--- assets/logic.js
+++ assets/logic.js
@@ -1,50 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Initialize Swiper with Coverflow effect
     var swiper = new Swiper('.swiper', {
-
+        initialSlide:1,
         slidesPerView: 1,
         freeMode:true,
         loop: true,
-        spaceBetween:32,
-        centeredSlides:true,
+        spaceBetween: 32,
+        effect: 'coverflow', // Add Coverflow effect
+        coverflowEffect: {
+            rotate: 0, // Rotate the slides
+            stretch: 0, // Stretch the slides
+            depth: 100, // Depth of the slides
+            modifier: .3, // Multiplier effect
+            slideShadows: true, // Enable slide shadows
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
         breakpoints: {
-            // when window width is >= 320px
             320: {
-    
+                // Customize for screens >= 320px
             },
-            // when window width is >= 480px
             480: {
-      
+                // Customize for screens >= 480px
             },
-            // when window width is >= 640px
             996: {
-                freeMode:true,
-                slidesPerView: 2,
-                spaceBetween:32,
-                loop:true,
-
-
-
+                initialSlide:1,
+                slidesPerView: 'auto',
+                spaceBetween: 32,
+                loop: true,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
             }
-          }
+        }
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function showSideBar() {
     const sidebar = $('.sidebar');
